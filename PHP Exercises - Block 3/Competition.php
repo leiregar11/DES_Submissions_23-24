2627a875e5695d34c43ab40cbf97ae41d3a3fed5
Leire Garmendia <lgarmendiadi20dw@ikzubirimanteo.com>
2023-09-27 14:28:05 +0200

    b3

---
+++ PHP Exercises - Block 3/Competition.php
@@ -0,0 +1,87 @@
+<?php
+include "Runner.php";
+class Competition{
+    private $runners;
+
+    public function __construct() {
+        $this->runners = [];
+    }
+
+    public function addRunner($runner) {
+        $code = $runner->getCode();
+        $this->runners[$code] = $runner;
+    }
+
+    public function addRaceToRunner($code, $time) {
+        if (array_key_exists($code, $this->runners)) {
+            $runner = $this->runners[$code];
+            $runner->addRace($time);
+        }
+    }
+
+    public function getAvrFirstRace() {
+        $sumRaces = 0;
+        $contRaces = 0;
+        foreach ($this->runners as $runner) {
+            $runnerRaces = $runner->getRaces();
+            if (count($runnerRaces) >= 1) {
+                $sumRaces += $runnerRaces[0];
+                $contRaces++;
+            }
+        }
+    
+        if ($contRaces > 0) {
+            return "Average time of the 1st race: " . ($sumRaces / $contRaces) . " seconds<br>";
+        } else {
+            return "No runners have completed a race.<br>";
+        }
+    }
+
+    function getQuickestRace() {
+        $quickestRace = 10000;
+        $faster = "";
+        foreach ($this->runners as $runner) {
+            $runnerRaces = $runner->getRaces();
+            if (count($runnerRaces) >= 1) { // Fixed the condition here
+                foreach ($runnerRaces as $race) {
+                    if ($race < $quickestRace) {
+                        $quickestRace = $race;
+                        $faster = $runner->getName();
+                    }
+                }
+            }
+        }
+        return "Quickest runner: $faster<br>";
+    }
+
+    public function getSlowRunners() {
+        $runnersMore15s = [];
+
+        foreach ($this->runners as $runner) {
+            $slowRaces = array_filter($runner->getRaces(), function ($time) {
+                return $time > 15;
+            });
+
+            if (count($slowRaces) > 2) {
+                $runnersMore15s[] = $runner->getName();
+            }
+        }
+
+        return "Runners with more than 2 races > 15 seconds: " . implode(', ', $runnersMore15s) . "<br>";
+    }
+
+    public function getRunnersWithNameEndingE() {
+        $runnersWithE = [];
+
+        foreach ($this->runners as $runner) {
+            $name = $runner->getName();
+            if (substr($name, -1) === 'e') { // Fixed the condition here
+                $runnersWithE[] = $name;
+            }
+        }
+
+        return "Runners with names ending in 'e': " . implode(', ', $runnersWithE) . "<br>";
+    }
+}
+
+?>
---
+++ PHP Exercises - Block 3/Test.php
@@ -0,0 +1,40 @@
+<?php
+
+$competition = new Competition();
+
+$runner1 = new Runner('Alice', 'A123');
+$runner2 = new Runner('Bob', 'B456');
+
+$competition->addRunner($runner1);
+$competition->addRunner($runner2);
+
+try {
+    $runner1->addRace(10);
+    $runner1->addRace(20);
+    $runner1->addRace(30);
+    $runner1->addRace(5);
+    $runner1->addRace(40); // Commented this line to avoid exceeding 5 races
+
+    $runner2->addRace(15);
+    $runner2->addRace(18);
+    $runner2->addRace(22);
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "<br>";
+}
+
+$competition->addRaceToRunner('A123', 12);
+$competition->addRaceToRunner('B456', 19);
+
+$averageTime = $competition->getAvrFirstRace();
+echo $averageTime;
+
+$quickestRunner = $competition->getQuickestRace();
+echo $quickestRunner;
+
+$slowRunners = $competition->getSlowRunners();
+echo $slowRunners;
+
+$runnersWithE = $competition->getRunnersWithNameEndingE();
+echo $runnersWithE;
+
+?>
