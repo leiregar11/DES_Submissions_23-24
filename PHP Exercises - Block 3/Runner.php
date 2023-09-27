f0769b0c0e2472de627acc4c96f62f2df1201e36
Leire Garmendia <lgarmendiadi20dw@ikzubirimanteo.com>
2023-09-27 14:24:04 +0200

    b3

---
+++ PHP Exercises - Block 3/Runner.php
@@ -0,0 +1,38 @@
+<?php
+class Runner{
+    private $name;
+    private $code;
+    private $races;
+
+    public function __construct($name, $code) {
+        $this->name = $name;
+        $this->code = $code;
+        $this->races = [];
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getCode() {
+        return $this->code;
+    }
+
+    public function getRaces() {
+        return $this->races;
+    }
+
+    public function addRace($time) {
+        if (count($this->races) >= 5) {
+            throw new Exception("Runner already has 5 races.<br>");
+        }
+
+        if ($time < 5) {
+            throw new Exception("Race time must be at least 5 seconds.<br>");
+        }
+
+        $this->races[] = $time;
+    }
+    
+}
+?>
